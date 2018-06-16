dced1d87dfaeca05985eefc9d277326e49675390
karantyagi <tyagi.k@husky.neu.edu>
2018-06-16 11:36:30 -0400

    added session management to server

--- server.js
+++ server.js
@@ -1,8 +1,54 @@
 var express = require('express')
 var app = express()
 
+
+var session = require('express-session')
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string'
+}));
+
+
 app.get('/', function (req, res) {
     res.send('Hello World from express server !')
 })
+
+
+function setSession(req, res) {
+    var name = req.params['name'];
+    var value = req.params['value'];
+    req.session[name] = value;
+    res.send(req.session);
+}
+
+function getSession(req, res) {
+    var name = req.params['name'];
+    var value = req.session[name];
+    res.send(value);
+}
+
+
+function getSessionAll(req, res) {
+    res.send(req.session);
+}
+
+function resetSession(req, res) {
+    res.session.destroy();
+    res.send(200);
+}
+
+
+// Register several HTTP endpoints to test session
+
+app.get('/api/session/set/:name/:value',
+    setSession);
+app.get('/api/session/get/:name',
+    getSession);
+app.get('/api/session/get',
+    getSessionAll);
+app.get('/api/session/reset',
+    resetSession);
+
 
 app.listen(4000);
