b93d586daf50eb89d14a4c5673fe5efb70fca1ab
karantyagi <tyagi.k@husky.neu.edu>
2018-06-16 13:54:00 -0400

    configured CORS for authentication

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/web-dev-db');
 
 
-var app = express()
+var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
