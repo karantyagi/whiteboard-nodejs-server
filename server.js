0be7aa6d94fd1c47be6785b4062f6a901fac67e5
karantyagi <tyagi.k@husky.neu.edu>
2018-06-20 20:28:11 -0400

    configured mlab mongo database and heroku angular client

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/web-dev-db');
+// mongoose.connect('mongodb://localhost/web-dev-db');
+mongoose.connect('mongodb://admin:admin123@ds263460.mlab.com:63460/whiteboard-db');
 
 
 var app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
+        // "http://localhost:4200");
         "https://kt-whiteboard-angular-client.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
